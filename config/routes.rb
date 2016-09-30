2e8eafcb972a39640520fe10711f7df419f0c066
shemoreira <sheilanara.web@hotmail.com>
2016-09-30 09:55:51 -0300

    alterando css

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "profiles#index"
-
-
+  post 'profiles/:id' => 'skills#new'
 
 end
